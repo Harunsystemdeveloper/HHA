baf5112408d74ed16c49fe5902818f6a6ba8bf2a
Harunsystemdeveloper <harun.ali.syne24lin@edu.tucsweden.se>
2025-11-03 13:03:40 +0100

    Lade till backendfiler och konfiguration för Orchard Core

---
+++ backend/RestRoutes/SetupRoutes.cs
@@ -0,0 +1,20 @@
+namespace RestRoutes;
+
+public static class SetupRoutes
+{
+    public static void MapRestRoutes(this WebApplication app)
+    {
+        app.UseRestRoutesExceptionHandler();
+
+        // Inject script into admin pages
+        app.UseMiddleware<AdminScriptInjectorMiddleware>();
+
+        app.MapAuthEndpoints();
+        app.MapSystemRoutes();
+        app.MapMediaUploadRoutes();
+        app.MapGetRoutes();
+        app.MapPostRoutes();
+        app.MapPutRoutes();
+        app.MapDeleteRoutes();
+    }
+}
